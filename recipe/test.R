5ce5d11509f8bf084e1aec4946c652feb95bacd9
Ralf Weber <RJMW@users.noreply.github.com>
2018-08-22 12:54:52 +0100

    Update test.R

--- recipe/test.R
+++ recipe/test.R
@@ -1,8 +1,8 @@
 library(rmarkdown)
 library(knitr)
 library(tinytex)
-rmarkdown::render('test.Rmd',
-                  output_file =  'test.pdf',
-                  output_format = "pdf_document",output_options = list('keep_tex'=TRUE),
+rmarkdown::render('/Users/travis/miniconda3/pkgs/r-tinytex-0.6-r341h6115d3f_0/info/recipe/test.Rmd',
+                  output_file =  '/Users/travis/miniconda3/pkgs/r-tinytex-0.6-r341h6115d3f_0/info/recipe/test.pdf',
+                  output_format = "pdf_document", output_options = list('keep_tex'=TRUE),
                   clean = F
 )
